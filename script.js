71d6260ef427ec98f48d994c4d9ac20e878ec99d
AgustinVelazquez0 <152011142+AgustinVelazquez0@users.noreply.github.com>
2024-01-13 04:50:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+
+    if (distancia_skills >= 300) {
+        let habilidades = document.querySelectorAll(".progreso");
+
+        habilidades.forEach(function (barraSkill) {
+            let percent = barraSkill.getAttribute("data-percent");
+            barraSkill.style.width = percent + "%";
+        });
+    }
+}
+
+// Detecta el scrolling para aplicar la animación de la barra de habilidades
+window.addEventListener("scroll", efectoHabilidades);
